513f48d441dd1726b91b76fcd68564505023925e
Alexis Perin <93121502+alexisperin@users.noreply.github.com>
2022-09-17 12:19:43 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 10:19:48 by aperin            #+#    #+#             */
+/*   Updated: 2022/09/17 12:06:41 by aperin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+
+int	invalid_format(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strlen(str) != 31)
+		return (1);
+	while (i < 31)
+	{
+		if (i % 2 == 1 && str[i] != ' ')
+			return (1);
+		if (i % 2 == 0 && (str[i] < '1' || str[i] > '4'))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 2 || invalid_format(argv[1]))
+	{
+		ft_putstr("Error\n");
+		return (0);
+	}
+	return (0);
+}
